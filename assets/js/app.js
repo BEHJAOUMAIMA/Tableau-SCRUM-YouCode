a42d3f6a117e98f8e442521d6f2a1acdcfd026fc
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2022-10-18 18:17:28 +0100

    Ajout de Nouvels dossiers

---
+++ assets/js/app.js
@@ -0,0 +1,73 @@
+/**
+ * In this file app.js you will find all CRUD functions name.
+ * 
+ */
+
+ function createTask() {
+    // initialiser task form
+    let form = document.getElementById("#form");
+    // Afficher le boutton save
+
+    // Ouvrir modal form
+    
+}
+
+function saveTask() {
+    // Recuperer task attributes a partir les champs input
+
+    // Créez task object
+
+    // Ajoutez object au Array
+
+    // refresh tasks
+    
+}
+
+function editTask(index) {
+    // Initialisez task form
+
+    // Affichez updates
+
+    // Delete Button
+
+    // Définir l’index en entrée cachée pour l’utiliser en Update et Delete
+
+    // Definir FORM INPUTS
+
+    // Ouvrir Modal form
+}
+
+function updateTask() {
+    // GET TASK ATTRIBUTES FROM INPUTS
+
+    // Créez task object
+
+    // Remplacer ancienne task par nouvelle task
+
+    // Fermer Modal form
+
+    // Refresh tasks
+    
+}
+
+function deleteTask() {
+    // Get index of task in the array
+
+    // Remove task from array by index splice function
+
+    // close modal form
+
+    // refresh tasks
+}
+
+function initTaskForm() {
+    // Clear task form from data
+
+    // Hide all action buttons
+}
+
+function reloadTasks() {
+    // Remove tasks elements
+
+    // Set Task count
+}
