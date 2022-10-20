f84a74eee8f14fd274aea5a489bf331e5014c09c
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2022-10-20 17:27:47 +0100

    Ajout du fonction d'Affichage

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,25 @@
  * 
  */
 
- function createTask() {
-    // initialiser task form
-    let form = document.getElementById("#form");
-    // Afficher le boutton save
 
-    // Ouvrir modal form
-    
+
+let save = document.getElementById('Save');
+function checkFB(){
+    if (Bug.checked){
+        Check = Bug;
+    }
+    if(Feature.checked){
+        Check = Feature;
+    }
 }
+ function createTask() {
+    
+    
+  
+    
+      
+    }
+
 
 function saveTask() {
     // Recuperer task attributes a partir les champs input
@@ -67,7 +78,57 @@ function initTaskForm() {
 }
 
 function reloadTasks() {
-    // Remove tasks elements
+    document.getElementById('to-do-tasks').innerHTML = "";
+    document.getElementById('doing-tasks').innerHTML = "";
+    document.getElementById('done-tasks').innerHTML = "";
+    let selectStatus;
+    document.getElementById('to-do-tasks-count').innerHTML = 0;
+    document.getElementById('doing-tasks-count').innerHTML = 0;
+    document.getElementById('done-tasks-count').innerHTML = 0;
+    for (let index = 0; index < tasks.length; index++) {
+        if (tasks[index].status== 'To Do') {
+            selectStatus = document.getElementById('to-do-tasks');
+            document.getElementById('to-do-tasks-count').innerHTML++;
+        }
+        else if(tasks[index].status== 'In Progress'){
+            selectStatus = document.getElementById('doing-tasks');
+            document.getElementById('doing-tasks-count').innerHTML++;
+        }
+        else if(tasks[index].status== 'Done'){
+            selectStatus = document.getElementById('done-tasks');
+            document.getElementById('done-tasks-count').innerHTML++;
 
-    // Set Task count
+        }
+        
+
+        selectStatus.innerHTML += `
+        <button class="w-100 border-0 d-flex bg-white border-bottom">
+                  <div class="col-1 mt-1">
+                    <i class="bi bi-question-circle h4 text-green"></i>
+                  </div>
+                  <div class="text-start col-11 mt-1">
+                    <div class="fw-bold">
+                      ${tasks[index].title}
+                    </div>
+                    <div class="">
+                      <div class="">#${index+1} created in ${tasks[index].date}</div>
+                      <div
+                        class=""
+                        title="${tasks[index].title}"
+                      >
+                      ${tasks[index].title}
+                      </div>
+                    </div>
+                    <div class="h5">
+                      <span class="btn btn-sm btn-primary p-0 px-1">High</span>
+                      <span class="btn btn-sm btn-light text-black p-0 px-1"
+                        >Feature</span
+                      >
+                    </div>
+                  </div>
+        </button>
+        `;
+            
+        
+    }
 }
